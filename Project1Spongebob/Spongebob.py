b5cf78d293854716129c0360e77a649a7b5dbff1
Matt Brown <home@Matts-MacBook-Pro.local>
2019-10-08 23:45:29 -0400

    base logic framework added for squidward

--- Project1Spongebob/Spongebob.py
+++ Project1Spongebob/Spongebob.py
@@ -2,13 +2,18 @@ from pygame import *
 from time import *
 from PIL import Image
 
+
+def menu_sleep():
+    """Sleep for 3.5 seconds during the menu presentation"""
+    sleep(3.5)
+
+
+def squidward():
+    print("this is the squidward function")
+
+
 mixer.init()
-mixer.music.load('Music/Menu_Song.mp3')
-mixer.music.play(-1)
-starting_input = input("To begin, enter your name: ")
-while mixer.music.get_busy():
-    print("How would you like to play, " + starting_input + "?")
-    mixer.music.stop()
+"""
 print("Let's get started!")
 mixer.music.load('Music/Theme_Song.mp3')
 mixer.music.play()
@@ -35,4 +40,52 @@ while mixer.music.get_busy():
     print("Spongebob Squarepants!")
     sleep(10)
     mixer.music.stop()
-print("test")
+"""
+mixer.music.load('Music/Menu_Song.mp3')
+mixer.music.play(-1)
+
+print(
+    "There are so many adventures to go on, but SpongeBob is super busy today and can only choose form 4 of them! "
+    "Select a number to choose: ")
+menu_sleep()
+print("Press 1 to go work at the Krusty Krab")
+menu_sleep()
+print("Press 2 to go jellyfishing with Patrick")
+menu_sleep()
+print("Press 3 to play the clarinet with Squidward")
+menu_sleep()
+print("Press 4 to practice karate with Sandy")
+menu_sleep()
+print("To quit, just enter the letter q")
+menu_sleep()
+
+adventure_choice = ' '
+
+while adventure_choice != 'q':
+
+    adventure_choice = input("What adventure would you like to go on today?! ")
+
+    if adventure_choice == str(1):
+        print("krusty_krab function called")
+        # def krusty_krab function/method called
+    elif adventure_choice == str(2):
+        print("jellyfishing function called")
+        # def jellyfishing function/method called
+    elif adventure_choice == str(3):
+        print("squidward function called")
+        squidward()
+        #put this in its own method called play_again
+        play_again = input("Would you like to play again? Press q to quit or press enter to go on another adventure! ")
+        if play_again == 'q':
+            break
+        else:
+            print("Yay! Let's play again!!")
+    elif adventure_choice == str(4):
+        print("karate method called")
+        # def karate function/method called
+    elif adventure_choice == 'q':
+        print("Thanks for playing, we hope you had fun!!")
+    else:
+        print("Invalid input!")
+
+print("You did great!")
